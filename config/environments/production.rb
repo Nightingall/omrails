32547131ba9f6d2d2063677ee2fc219cf26ee8e1
Nightingall <nightingallh@gmail.com>
2016-10-11 13:10:10 +0100

    rename application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "omruby_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "omrails_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
